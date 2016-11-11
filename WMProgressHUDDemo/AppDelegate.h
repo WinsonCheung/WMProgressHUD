ddeef76baaf6187f419e68497ce6bfbdfaf8f0d2
Winson <tech@longkin.net>
2016-11-11 21:50:52 +0800

    Initial Commit

---
+++ WMProgressHUDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WMProgressHUDDemo
+//
+//  Created by Winson Cheung on 2016/11/11.
+//  Copyright © 2016年 Winson Cheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WMProgressHUDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMProgressHUDDemo
+//
+//  Created by Winson Cheung on 2016/11/11.
+//  Copyright © 2016年 Winson Cheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
